c763f9e526f66cffb8499f93cb74dbdb7095b874
nflores <Nahun.a.f.a90@gmail.com>
2023-06-12 06:33:06 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Tarea;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Tarea::factory(10000)->create();
+        //Tarea::factory(10000)->create();
+
+        $user = new User();
+        $user->name = "Administrador";
+        $user->email = "admin@gmail.com";
+        $user->password = '$2y$10$Y8woSBY4JAUYYQwA9crX6.XKZP1kGlFlB0LR5IhxBFWLc0vcYiJZy'; //12345678
+        $user->save();
     }
 }
